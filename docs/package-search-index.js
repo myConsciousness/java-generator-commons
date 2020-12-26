4e35b717827236b859ade3ee4470191e76239ef1
Kato Shinya <kato.shinya.dev@gmail.com>
2020-12-26 23:36:53 +0900

    add

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.thinkit.generator.common.duke.catalog"},{"l":"org.thinkit.generator.common.duke.dto"},{"l":"org.thinkit.generator.common.duke.factory"},{"l":"org.thinkit.generator.common.duke.formatter"},{"l":"org.thinkit.generator.common.duke.strategy"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
